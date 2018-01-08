ba30dad081befcacfe1580c748f343e44970f91e
Marguerite Pometko <marguerite.pometko@gmail.com>
2018-01-08 19:55:07 +0100

    Initial layout & dummy data

---
+++ README.md
@@ -0,0 +1,18 @@
+# Config
+
+
+# Front matters
+
+## General
+
+
+
+## Page
+
+## Article
+
+
+# TODO
+
+_config.yml customization
+rename image assets !
---
+++ assets/js/main.js
@@ -0,0 +1,13 @@
+$(function () {
+  // Get all "navbar-burger" elements
+  var $navbarBurgers = $('.navbar-burger');
+
+  $navbarBurgers.on('click', function (event) {
+    var $el = $(event.delegateTarget);
+    var $target = $('#' + $el.data('target'));
+
+    // Toggle the class on both the "navbar-burger" and the "navbar-menu"
+    $el.toggleClass('is-active');
+    $target.toggleClass('is-active');
+  });
+});
